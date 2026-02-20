80fa7adf19eee3f7ddbc84d4c99bc91300ad76f8
Michael <mv.santay@sbcglobal.net>
2026-02-19 19:17:35 -0600

    added momo meow button

--- GTKEOE.js
+++ GTKEOE.js
@@ -2,4 +2,22 @@ const text = document.getElementById("color-1");
 
 text.addEventListener("click", function() {
     text.style.color = "orange";
+}); 
+
+
+const title = document.getElementById("color-1");
+const momoImage = document.getElementById("momoImage");
+const button = document.getElementById("funFactButton");
+const momoAudio = document.getElementById("momoAudio");
+
+title.addEventListener("click", () => {
+  title.style.color = "orange";
 });
+
+function playSound() {
+  momoAudio.currentTime = 0;
+  momoAudio.play();
+}
+
+momoImage.addEventListener("click", playSound);
+button.addEventListener("click", playSound);
